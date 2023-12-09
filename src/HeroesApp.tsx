7e7389a078c96d91cedd8ed6f88866fa4cb66325
sanchezmotta <sanchezmotta@google.com>
2023-12-09 13:10:51 -0500

    Initial structure of the spa

---
+++ src/HeroesApp.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react'
+import { AppRouter } from './router/AppRouter'
+
+export const HeroesApp: FunctionComponent = () => {
+  return (
+    <>
+      <AppRouter />
+    </>
+  )
+}
